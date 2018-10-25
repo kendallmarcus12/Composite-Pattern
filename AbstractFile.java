5877c1d534b48cccc8fdf0e89537a489e8357d4f
Kendall Elmore <kendallmarcus12@gmail.com>
2018-10-25 11:50:42 -0400

    Files needed

---
+++ AbstractFile.java
@@ -0,0 +1,3 @@
+interface AbstractFile{
+    public void ls();    
+}
---
+++ CompositeDemo.java
@@ -0,0 +1,9 @@
+class CompositeDemo
+{
+    public static StringBuffer g_indent = new StringBuffer();
+
+    public static void main(String[] args)
+    {
+        //Your code goes here
+    }
+}
